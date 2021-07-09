cd26a1d626c942499f76095260b659632ea9faff
Ardis Lu <ardisglu@gmail.com>
2021-07-08 23:14:27 -0700

    Follow redirects in curl

--- ipfs-check.sh
+++ ipfs-check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 content="QmQG6T9EFHzBxor5s4J5XDmF9i65mf3SFwaGvZFU3xmsXf"
-curl -i $(shuf -n 1 gateways.txt | sed "s/{0}/$content/; s/\r$//")
+curl -i -L $(shuf -n 1 gateways.txt | sed "s/{0}/$content/; s/\r$//")
