f4edc44b3fdf4cad1c61844ec2c09fe592e3b4da
Ardis Lu <ardisglu@gmail.com>
2024-07-15 21:57:39 -0700

    Rename "IPFS hash" to "IPFS CID"

--- ipfs-check.ps1
+++ ipfs-check.ps1
@@ -1,6 +1,6 @@
-$content = $args[0] # Can't directly reference $args[0] in a ForEach-Object block because there is another $args scoped inside the block
-if ($null -eq $content) {
-  Write-Output "No IPFS hash provided"
+$cid = $args[0] # Can't directly reference $args[0] in a ForEach-Object block because there is another $args scoped inside the block
+if ($null -eq $cid) {
+  Write-Output "No IPFS content identifier (CID) provided"
   exit 1
 }
-Invoke-WebRequest $(Get-Content gateways.txt | Get-Random | ForEach-Object {$_.replace("{0}", $content)})
+Invoke-WebRequest $(Get-Content gateways.txt | Get-Random | ForEach-Object {$_.replace("{0}", $cid)})
--- ipfs-check.sh
+++ ipfs-check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-  echo "No IPFS hash provided"
+  echo "No IPFS content identifier (CID) provided"
   exit 1
 fi
 curl -i -L $(shuf -n 1 gateways.txt | sed "s/{0}/$1/; s/\r$//")
